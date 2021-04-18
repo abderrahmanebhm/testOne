735ce098842335d1412b9a9cde525dc631bf1a11
Abderrahmane <abdeled600@gmail.com>
2021-04-18 22:08:27 +0000

    modification du message -18

--- main.c
+++ main.c
@@ -3,13 +3,16 @@
 int main()
 {
 int age = 18;
-   if (age >= 18)
-   {
-       printf("welcome\n");
-   }
-   else
-   {
-       printf("Warning you ar not majeur\n");
-   }
-   return 0;
+  
+  if (age >= 18)
+  {
+    printf("welcome\n");
+  }
+  else
+  {
+    printf("Warning\n");
+  }
+
+  return 0;
 }
+
